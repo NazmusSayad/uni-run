370c6805ecec42b18a48c64179006e4292361b69
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-24 01:50:00 +0600

    chore: improve TypeScript runtime handling and configuration updates
    - Enhanced TypeScript runtime support in default-runtime.ts with improved async parsing logic.
    - Updated user configuration handling for better execution flow.

---
+++ src/helpers/lib.ts
@@ -0,0 +1,18 @@
+import { randomUUID } from 'crypto'
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+
+export function getTempOutputFilePath(slug?: string) {
+  const ext = process.platform === 'win32' ? '.exe' : ''
+
+  const dirPath = [slug ?? randomUUID(), process.pid, process.ppid].join('-')
+  const tempDir = path.join(os.tmpdir(), dirPath)
+
+  if (fs.existsSync(tempDir)) {
+    fs.rmSync(tempDir, { recursive: true })
+  }
+
+  fs.mkdirSync(tempDir, { recursive: true })
+  return path.join(tempDir, 'output' + ext)
+}
